3702e6e4ad32f693f58f6bdc14099c04bc392160
Tharikh <tharikhlegend@gmail.com>
2021-11-23 23:22:08 +0530

    basic structure

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navs from './Navs';
+class Main extends React.Component{
+    render(){
+        return(
+            <div>
+                <Navs />
+            </div>
+        );
+    }
+}
+
+export default Main;
