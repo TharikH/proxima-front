2edf35e7b08e53ba065e09462957f917ce059944
shahil <shahilkp6@gmail.com>
2021-11-24 12:24:37 +0530

    Login

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import Navs from './Navs';
+import Login from './Login';
+import Signup from './Signup'
+import {BrowserRouter as Router,Switch, Route} from 'react-router-dom';
 class Main extends React.Component{
     render(){
         return(
-            <div>
-                <Navs />
+            <div> 
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        <Login />
+                    </Route>
+                    <Route path="/Signup">
+                        <Signup />
+                    </Route>
+
+
+
+                </Switch>
+               
+            </Router>   
             </div>
         );
     }
